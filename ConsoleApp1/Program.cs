31aff53092dae5ebe6a3556336d9968de9b548a5
hv <hv@PM304-08>
2023-08-18 20:52:43 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*            int[] mangSoNguyen = { 1, 5, 3, 7, 15 };
+                        int t = 0;
+                        int n = 0;
+                        Console.WriteLine("Do dai cua mang la: {0}", mangSoNguyen.Length);
+                        int sum = 0;
+                        for (int i = 0; i < mangSoNguyen.Length; i++) 
+                        {
+                            sum += mangSoNguyen[i];
+                        }
+                        Console.WriteLine("Tong cua mang for: " + sum);
+                        foreach (var item in mangSoNguyen)
+                        {
+                            t = t + item;
+                        }
+                        Console.WriteLine("Tong cua mang foreach: " + sum);
+                        n = mangSoNguyen.Sum();
+                        Console.WriteLine("Tong cua mang sum: " + sum);
+                        //tim so lon nhat
+                        int max = mangSoNguyen.Max();
+                        Console.WriteLine("So lon nhat: " + max);
+                        int min = mangSoNguyen.Min();
+                        Console.WriteLine("So nho nhat: " + min);
+                        int a = mangSoNguyen.Average();
+                        Console.WriteLine("So trung binh: " + a);*/
+
+            /*            Console.Write("Nhap vao so luong: ");
+                        int n = Convert.ToInt32(Console.ReadLine());
+
+                        int[] arr = new int[n];
+                        int sum = 0;
+
+                        for (int i = 0; i < n; i++)
+                        {
+                            Console.Write("So: ");
+                            arr[i] = Convert.ToInt32(Console.ReadLine());
+                            sum += arr[i];
+                        }
+                        Console.WriteLine("Tong mang la: " + sum);*/
+
+            /*            Console.WriteLine("Nhap so luong: ");
+                        int n = int.Parse(Console.ReadLine());
+            */
+            int[,] myArray = new int[] 
+
+
+            Console.ReadKey();
+        }
+    }
+}
